80599cc740543d350db7099e21025679c8ee0b05
Chandra Shekhar Shil <shekhar40@users.noreply.github.com>
2021-08-02 17:36:07 +0600

    Update out10.c

--- out10.c
+++ out10.c
@@ -12,20 +12,5 @@ int main()
     return 0;
 }
 
---------------------------------------------------------------------
-  
-  #include <stdio.h>
-#include <stdlib.h>
 
-
-int main()
-{
-  int i;
-	for(i=0;i<5;i++)
-	{
-		if(i==3)
-		continue;
-		printf("%d",i);
-	}
-    return 0;
-}
+ 
